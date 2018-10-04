247218568b8f809653de6f557b9555021aacc7a0
server <server@DESKTOP-0CQ10LV>
2018-10-04 09:29:37 -0700

    its a new class added by prasanth

---
+++ TestNG/src/main/java/TestNG/TestNG/test3.java
@@ -0,0 +1,10 @@
+package TestNG.TestNG;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the test3");
+	}
+
+}
